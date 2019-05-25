9d839300b9a816af4fb9922dc0e4027d79ad995d
Raquel Monserrat Carlos Perez <monse@99.200-57-1.bestelclientes.com.mx>
2019-05-24 21:10:58 -0500

    algoritmos clase 5

---
+++ algoritmos_js_node/hambre.js
@@ -0,0 +1,8 @@
+const hambre = true
+
+if(hambre === true){
+    console.log("Joel tiene hambre, cuidado")
+}
+else{
+    console.log("Joel esta feliz, todos 10")
+}
---
+++ algoritmos_js_node/nivel.js
@@ -0,0 +1,11 @@
+const nivel = 20
+
+if(nivel < 10){
+    console.log("Tu nivel es aprendiz")
+}
+else if(nivel >=10 && nivel<20){
+    console.log("Tu nivel es Acrobata")
+}
+else if(nivel >= 20){
+    console.log("Tu nivel es ninja")
+}
