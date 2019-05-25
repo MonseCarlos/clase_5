1bf6fd1338b8b61c2b7cc31e55aaa0584d492eca
Raquel Monserrat Carlos Perez <monse@99.200-57-1.bestelclientes.com.mx>
2019-05-24 21:16:47 -0500

    clase 5

---
+++ algoritmos_js_node/horario.js
@@ -0,0 +1,11 @@
+const dia = "LUNES"
+
+if(dia === "LUNES" || dia === "MIERCOLES"){
+    console.log("Tienes historia")
+}
+else if(dia == "JUEVES"){
+    console.log("Tienes esgrima")
+}
+else{
+    console.log("Tienes el día de descanso")
+}
